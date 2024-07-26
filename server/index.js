53c72c46e45ae49ffb743bf757081e92dbd3cb44
Yasemin <ykhanmoradi@gmail.com>
2024-07-26 10:54:26 -0600

    added authentication

--- server/index.js
+++ server/index.js
@@ -8,6 +8,9 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import { register } from "./controllers/auth.js";
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -33,6 +36,13 @@ const storage = multer.diskStorage({
     }
 });
 const upload = multer ({ storage });
+
+/* ROUTES WITH FILES*/
+app.post("/auth/register", upload.single("picture"), register);
+
+/* ROUTES */
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
 
 
 /* MONGOOSE SETUP */
